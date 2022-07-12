cf2c938d34a162c9b9769d63a1c89c3d16d9be52
Fatta Salvatore <fatta.salvatore02@gmail.com>
2022-07-12 12:54:42 +0200

    Aggiunte servlet per l'inserimento delle Auto

---
+++ src/main/java/it/rjcsoft/provawebapp/controller/Inserimento.java
@@ -0,0 +1,59 @@
+package it.rjcsoft.provawebapp.controller;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import it.rjcsoft.provawebapp.model.DBdriver;
+import it.rjcsoft.provawebapp.model.User;
+import it.rjcsoft.provawebapp.model.UsersDB;
+
+/**
+ * Servlet implementation class Inserimento
+ */
+@WebServlet("/Inserimento")
+public class Inserimento extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static final String Pagename = "/WEB-INF/form_inserimento.jsp";
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Inserimento() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		DBdriver db = DBdriver.getInstance();
+		Connection conn = db.openConnection();
+		UsersDB user = new UsersDB(conn);
+		
+		ArrayList<User> users = user.SelectUserLimitOffset(10,1);
+		request.setAttribute("proprietari", users);
+		
+		
+		
+		RequestDispatcher disp = request.getRequestDispatcher (Pagename);
+		disp.forward(request,response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
